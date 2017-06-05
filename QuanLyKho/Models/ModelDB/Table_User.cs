83cf54f319328c8c3382ffeae5c8b2b8b177ece0
ducanh96 <rogerducanh@gmail.com>
2017-06-05 14:54:43 +0700

    moi nhat

--- QuanLyKho/Models/ModelDB/Table_User.cs
+++ QuanLyKho/Models/ModelDB/Table_User.cs
@@ -11,7 +11,8 @@ namespace QuanLyKho.Models.ModelDB
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Table_User
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -24,6 +25,7 @@ namespace QuanLyKho.Models.ModelDB
     
         public int UserId { get; set; }
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string UserPassword { get; set; }
         public Nullable<bool> IsActive { get; set; }
     
